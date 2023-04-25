8f4acdb869800952a3e6ca986b99f44a78a1bd17
dima.avdeev <dima.avdeev@jetbrains.com>
2023-04-25 19:12:41 +0400

    try to handle exception

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,31 +1,55 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.LocalWindowExceptionHandlerFactory
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowExceptionHandler
+import androidx.compose.ui.window.WindowExceptionHandlerFactory
 import androidx.compose.ui.window.application
 
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() = application {
+    var libraryErrorMessage: String? by remember { mutableStateOf(null) }
+    Window(onCloseRequest = ::exitApplication) {
+        Column(Modifier.fillMaxSize()) {
+            Text("Top level Application")
+            libraryErrorMessage?.let {
+                Text("Library error: $it")
+            }
+            Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+                Box(Modifier.background(Color.LightGray).padding(20.dp)) {
+                    CompositionLocalProvider(
+                        LocalWindowExceptionHandlerFactory provides WindowExceptionHandlerFactory {
+                            WindowExceptionHandler {
+                                libraryErrorMessage = it.message
+                            }
+                        }
+                    ) {
+
+                        Library1()
+
+                    }
+                }
+
+            }
         }
     }
 }
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+@Composable
+fun Library1() {
+    Button({
+        throw Error("Simulate error")
+    }) {
+        Text("Throw exception")
     }
 }
